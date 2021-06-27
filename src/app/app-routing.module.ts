e3c2a02657e9d17a5e77b2e6bdf3ca2306277b0d
Ayush <ayush.sharma@pristyncare@gmail.com>
2021-06-27 14:10:27 +0530

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashComponent } from './dash/dash.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dash/dash.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashComponent } from './dash.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DashComponent]
+})
+export class DashModule { }
